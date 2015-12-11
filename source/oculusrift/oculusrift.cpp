c44bfaf5ec81f19296febd684957539c9309790c
Graham Wakefield <grrrwaaa@gmail.com>
2015-12-11 13:13:16 -0500

    oculus project

---
+++ source/oculusrift/oculusrift.cpp
@@ -0,0 +1,73 @@
+// a bunch of likely Max includes:
+extern "C" {
+#include "ext.h"
+#include "ext_obex.h"
+#include "ext_dictionary.h"
+#include "ext_dictobj.h"
+#include "ext_systhread.h"
+	
+#include "z_dsp.h"
+	
+#include "jit.common.h"
+#include "jit.gl.h"
+}
+
+#include <new> // for in-place constructor
+
+static t_class * max_class = 0;
+
+class Example {
+public:
+	t_object ob; // max objExamplet, must be first!
+	
+	Example() {
+		
+	}
+	
+	~Example() {
+		
+	}
+};
+
+void * example_new(t_symbol *s, long argc, t_atom *argv) {
+	Example *x = NULL;
+	if ((x = (Example *)object_alloc(max_class))) {
+		
+		x = new (x) Example();
+		
+		// apply attrs:
+		attr_args_process(x, (short)argc, argv);
+		
+		// invoke any initialization after the attrs are set from here:
+		
+	}
+	return (x);
+}
+
+void example_free(Example *x) {
+	x->~Example();
+}
+
+void example_assist(Example *x, void *b, long m, long a, char *s)
+{
+	if (m == ASSIST_INLET) { // inlet
+		sprintf(s, "I am inlet %ld", a);
+	}
+	else {	// outlet
+		sprintf(s, "I am outlet %ld", a);
+	}
+}
+
+void ext_main(void *r)
+{
+	t_class *c;
+	
+	c = class_new("example", (method)example_new, (method)example_free, (long)sizeof(Example),
+				  0L /* leave NULL!! */, A_GIMME, 0);
+	
+	/* you CAN'T call this from the patcher */
+	class_addmethod(c, (method)example_assist,			"assist",		A_CANT, 0);
+	
+	class_register(CLASS_BOX, c);
+	max_class = c;
+}
