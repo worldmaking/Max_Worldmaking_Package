031f6b2b70a2f3e52d140d409f6197c9f62fb547
Graham Wakefield <grrrwaaa@gmail.com>
2015-12-11 17:12:38 -0500

    Added LIbOVR

---
+++ help/oculusrift.maxhelp
@@ -0,0 +1,453 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 7,
+			"minor" : 0,
+			"revision" : 6,
+			"architecture" : "x86",
+			"modernui" : 1
+		}
+,
+		"rect" : [ 601.0, 307.0, 1023.0, 624.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 1,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 1,
+		"objectsnaponopen" : 1,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"lefttoolbarpinned" : 0,
+		"toptoolbarpinned" : 0,
+		"righttoolbarpinned" : 0,
+		"bottomtoolbarpinned" : 0,
+		"toolbars_unpinned_last_save" : 0,
+		"tallnewobj" : 0,
+		"boxanimatetime" : 200,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"style" : "",
+		"subpatcher_template" : "",
+		"boxes" : [ 			{
+				"box" : 				{
+					"id" : "obj-21",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 564.0, 337.0, 31.0, 22.0 ],
+					"presentation_rect" : [ 564.0, 339.0, 0.0, 0.0 ],
+					"style" : "",
+					"text" : "perf"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-19",
+					"linecount" : 4,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 385.0, 46.0, 150.0, 62.0 ],
+					"style" : "",
+					"text" : "first bang to get predicted tracking state, then render, then send the rendered texture"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-12",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 3,
+					"outlettype" : [ "bang", "bang", "bang" ],
+					"patching_rect" : [ 332.0, 86.0, 44.0, 22.0 ],
+					"style" : "",
+					"text" : "t b b b"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-15",
+					"maxclass" : "button",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 225.0, 319.0, 24.0, 24.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 332.0, 413.0, 34.0, 22.0 ],
+					"style" : "",
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-18",
+					"linecount" : 4,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 164.0, 248.0, 99.0, 62.0 ],
+					"style" : "",
+					"text" : "need to bang a render in jitter to get the texture uploaded"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-17",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "bang", "" ],
+					"patching_rect" : [ 164.0, 220.0, 80.0, 22.0 ],
+					"style" : "",
+					"text" : "jit.gl.render o"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-16",
+					"maxclass" : "jit.pwindow",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 366.0, 267.0, 80.0, 60.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-14",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 3,
+					"outlettype" : [ "bang", "bang", "erase" ],
+					"patching_rect" : [ 164.0, 186.0, 67.0, 22.0 ],
+					"style" : "",
+					"text" : "t b b erase"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-13",
+					"maxclass" : "button",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 164.0, 84.0, 24.0, 24.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-11",
+					"maxclass" : "toggle",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "int" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 195.0, 50.0, 24.0, 24.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-9",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 195.0, 86.0, 81.0, 22.0 ],
+					"style" : "",
+					"text" : "qmetro 13.33"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-8",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "jit_matrix", "" ],
+					"patching_rect" : [ 332.0, 135.0, 135.0, 22.0 ],
+					"style" : "",
+					"text" : "jit.noise 4 char 400 400"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-7",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "jit_gl_texture", "" ],
+					"patching_rect" : [ 332.0, 228.0, 229.0, 22.0 ],
+					"style" : "",
+					"text" : "jit.gl.texture o @name torift @rectangle 0"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-6",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "bang", "" ],
+					"patching_rect" : [ 84.0, 220.0, 73.0, 22.0 ],
+					"style" : "",
+					"text" : "jit.window o"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-5",
+					"linecount" : 4,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 647.0, 142.0, 150.0, 62.0 ],
+					"style" : "",
+					"text" : "Jitter doesn't really provide a way to use external GPU memory that it didn't allocate itself"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"linecount" : 5,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 647.0, 216.0, 176.0, 75.0 ],
+					"style" : "",
+					"text" : "We can generate a texture within Jitter, pass it to the oculus object, and ask it to copy this texture over to the Oculus memory"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-3",
+					"linecount" : 3,
+					"maxclass" : "comment",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 647.0, 86.0, 150.0, 48.0 ],
+					"style" : "",
+					"text" : "Oculus driver gives us GPU texture memory to render our scene into."
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 332.0, 365.0, 59.0, 22.0 ],
+					"style" : "",
+					"text" : "oculusrift"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"attr" : "rectangle",
+					"id" : "obj-20",
+					"maxclass" : "attrui",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 366.0, 192.0, 150.0, 22.0 ],
+					"style" : ""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-10",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 485.0, 337.0, 50.0, 22.0 ],
+					"style" : "",
+					"text" : "info"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-11", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-12", 2 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-8", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-12", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-14", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-13", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-12", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-14", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-17", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-14", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-15", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-20", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-21", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-16", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-14", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-9", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [ 			{
+				"name" : "oculusrift.mxe",
+				"type" : "iLaF"
+			}
+ ],
+		"embedsnapshot" : 0
+	}
+
+}
--- source/oculusrift/oculusrift.cpp
+++ source/oculusrift/oculusrift.cpp
@@ -1,3 +1,14 @@
+/*
+
+	Might be important:
+		https://forums.oculus.com/viewtopic.php?f=20&t=24361&p=283304&hilit=opengl+0.6#p283304
+	Might be useful:
+		https://forums.oculus.com/viewtopic.php?f=39&t=91&p=277330&hilit=opengl+0.6#p277330
+
+
+*/
+
+
 // a bunch of likely Max includes:
 extern "C" {
 #include "ext.h"
@@ -14,41 +25,299 @@ extern "C" {
 
 #include <new> // for in-place constructor
 
+#include "OVR_CAPI.h"
+#include "OVR_CAPI_GL.h"
+
+
 static t_class * max_class = 0;
 
-class Example {
+static t_symbol * ps_glid;
+
+class oculusrift {
 public:
-	t_object ob; // max objExamplet, must be first!
-	
-	Example() {
-		
+	t_object ob; // must be first!
+	void * outlet_msg;
+
+	ovrSession session;
+	ovrGraphicsLuid luid;
+	ovrEyeRenderDesc eyeRenderDesc[2];
+	ovrVector3f      hmdToEyeViewOffset[2];
+	ovrLayerEyeFov layer;
+	ovrSwapTextureSet * pTextureSet;
+	ovrTexture * mirrorTexture;
+
+	long long frameIndex;
+
+	int perfMode;
+
+	oculusrift() {
+
+		outlet_msg = outlet_new(&ob, NULL);
+
+		frameIndex = 0;
+		perfMode = 0;
+
+		ovrResult result;
+
+		result = ovr_Create(&session, &luid);
+		if (OVR_FAILURE(result))
+		{
+			error("failed to create session -- is the HMD plugged in?");
+			return;
+		}
+
+		object_post(&ob, "LibOVR runtime version %s", ovr_GetVersionString());
+
+		ovrHmdDesc hmd = ovr_GetHmdDesc(session);
+		ovrSizei resolution = hmd.Resolution;
+
+		post("created session with resolution %d %d\n", resolution.w, resolution.h);
+
+		// just use default tracking options for now -- nothing to do for ovr_ConfigureTracking()
+
+		//  - Use hmd members and ovr_GetFovTextureSize() to determine graphics configuration
+		//    and ovr_GetRenderDesc() to get per-eye rendering parameters.
+		// Configure Stereo settings.
+		float pixelDensity = 1.f;
+		ovrSizei recommenedTex0Size = ovr_GetFovTextureSize(session, ovrEye_Left, hmd.DefaultEyeFov[0], pixelDensity);
+		ovrSizei recommenedTex1Size = ovr_GetFovTextureSize(session, ovrEye_Right, hmd.DefaultEyeFov[1], pixelDensity);
+		ovrSizei bufferSize;
+		// assumes a single shared texture for both eyes:
+		bufferSize.w = recommenedTex0Size.w + recommenedTex1Size.w;
+		bufferSize.h = max(recommenedTex0Size.h, recommenedTex1Size.h);
+
+		post("recommended texture resolution %d %d\n", bufferSize.w, bufferSize.h);
+
+
+		//  - Allocate render target texture sets with ovr_CreateSwapTextureSetD3D11() or
+		//    ovr_CreateSwapTextureSetGL().
+		pTextureSet = 0;
+		// TODO problem here: Jitter API GL headers don't export GL_SRGB8_ALPHA8
+		// might also need  GL_EXT_framebuffer_sRGB for the copy
+		// "your application should call glEnable(GL_FRAMEBUFFER_SRGB); before rendering into these textures."
+		// I'm not sure if just passing in the constant like this will actually work
+		// Even though it is not recommended, if your application is configured to treat the texture as a linear 
+		// format (e.g.GL_RGBA) and performs linear - to - gamma conversion in GLSL or does not care about gamma - 
+		// correction, then:
+		// Request an sRGB format(e.g.GL_SRGB8_ALPHA8) swap - texture - set.
+		// Do not call glEnable(GL_FRAMEBUFFER_SRGB); when rendering into the swap texture.
+#ifndef GL_SRGB8_ALPHA8
+#define GL_SRGB8_ALPHA8                   0x8C43
+#endif
+		result = ovr_CreateSwapTextureSetGL(session, GL_SRGB8_ALPHA8, bufferSize.w, bufferSize.h, &pTextureSet);
+		//result = ovr_CreateSwapTextureSetGL(session, GL_RGBA8, bufferSize.w, bufferSize.h, &pTextureSet);
+		if (result != ovrSuccess) {
+			ovrErrorInfo errInfo;
+			ovr_GetLastErrorInfo(&errInfo);
+			object_error(&ob, "failed to create texture set: %s", errInfo.ErrorString);
+			
+			// Sample texture access:
+			//ovrGLTexture* tex = (ovrGLTexture*)&pTextureSet->Textures[i];
+			//glBindTexture(GL_TEXTURE_2D, tex->OGL.TexId);
+			//...
+			//glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		result = ovr_CreateMirrorTextureGL(session, GL_SRGB8_ALPHA8, bufferSize.w, bufferSize.h, &mirrorTexture);
+		if (result != ovrSuccess) {
+			ovrErrorInfo errInfo;
+			ovr_GetLastErrorInfo(&errInfo);
+			object_error(&ob, "failed to create mirror texture: %s", errInfo.ErrorString);
+
+			// Sample texture access:
+			//ovrGLTexture* tex = (ovrGLTexture*)mirrorTexture;
+			//glBindTexture(GL_TEXTURE_2D, tex->OGL.TexId);
+			//...
+			//glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		// Initialize VR structures, filling out description.
+		eyeRenderDesc[0] = ovr_GetRenderDesc(session, ovrEye_Left, hmd.DefaultEyeFov[0]);
+		eyeRenderDesc[1] = ovr_GetRenderDesc(session, ovrEye_Right, hmd.DefaultEyeFov[1]);
+		hmdToEyeViewOffset[0] = eyeRenderDesc[0].HmdToEyeViewOffset;
+		hmdToEyeViewOffset[1] = eyeRenderDesc[1].HmdToEyeViewOffset;
+
+		// Initialize our single full screen Fov layer.
+		layer.Header.Type = ovrLayerType_EyeFov;
+		layer.Header.Flags = 0;
+		layer.ColorTexture[0] = pTextureSet;
+		layer.ColorTexture[1] = pTextureSet;
+		layer.Fov[0] = eyeRenderDesc[0].Fov;
+		layer.Fov[1] = eyeRenderDesc[1].Fov;
+		layer.Viewport[0].Pos.x = 0;
+		layer.Viewport[0].Pos.y = 0;
+		layer.Viewport[0].Size.w = bufferSize.w / 2;
+		layer.Viewport[0].Size.h = bufferSize.h;
+		layer.Viewport[1].Pos.x = bufferSize.w / 2;
+		layer.Viewport[1].Pos.y = 0;
+		layer.Viewport[1].Size.w = bufferSize.w / 2;
+		layer.Viewport[1].Size.h = bufferSize.h;
+
+		// ld.RenderPose and ld.SensorSampleTime are updated later per frame.
+	}
+
+	void info() {
+		ovrHmdDesc hmd = ovr_GetHmdDesc(session);
+		t_atom a[2];
+
+		// TODO complete list of useful info from https://developer.oculus.com/documentation/pcsdk/latest/concepts/dg-sensor/
+#define HMD_CASE(T) case T: { \
+            atom_setsym(a, gensym( #T )); \
+            outlet_anything(outlet_msg, gensym("hmdType"), 1, a); \
+            break; \
+	        }
+		switch (hmd.Type) {
+			HMD_CASE(ovrHmd_DK1)
+			HMD_CASE(ovrHmd_DKHD)
+			HMD_CASE(ovrHmd_DK2)
+		default: {
+				atom_setsym(a, gensym("unknown"));
+				outlet_anything(outlet_msg, gensym("Type"), 1, a);
+			}
+		}
+#undef HMD_CASE
+
+		// note serial:
+		atom_setsym(a, gensym(hmd.SerialNumber));
+		outlet_anything(outlet_msg, gensym("serial"), 1, a);
+
+		atom_setsym(a, gensym(hmd.Manufacturer));
+		outlet_anything(outlet_msg, gensym("Manufacturer"), 1, a);
+		atom_setsym(a, gensym(hmd.ProductName));
+		outlet_anything(outlet_msg, gensym("ProductName"), 1, a);
+
+		atom_setlong(a, hmd.FirmwareMajor);
+		atom_setlong(a + 1, hmd.FirmwareMinor);
+		outlet_anything(outlet_msg, gensym("Firmware"), 2, a);
 	}
 	
-	~Example() {
+	~oculusrift() {
+		if (session) ovr_Destroy(session);
+		if (pTextureSet) ovr_DestroySwapTextureSet(session, pTextureSet);
+		if (mirrorTexture) ovr_DestroyMirrorTexture(session, mirrorTexture);
+	}
+
+	/*
+	Frame rendering typically involves several steps: 
+	- obtaining predicted eye poses based on the headset tracking pose, 
+	- rendering the view for each eye and, finally, 
+	- submitting eye textures to the compositor through ovr_SubmitFrame. 
+	*/
+
+	void bang() {
+		// Query the HMD for the current tracking state.
+
+		// Get both eye poses simultaneously, with IPD offset already included.
+		double displayMidpointSeconds = ovr_GetPredictedDisplayTime(session, frameIndex);
+		ovrTrackingState ts = ovr_GetTrackingState(session, displayMidpointSeconds, ovrTrue);
+		ovr_CalcEyePoses(ts.HeadPose.ThePose, hmdToEyeViewOffset, layer.RenderPose);
+
+		/*
+		TODO
+		If the time passed into ovr_GetTrackingState is the current time or earlier,
+		the tracking state returned will be based on the latest sensor readings with
+		no prediction. In a production application, however, you should use the real-time
+		computed value returned by GetPredictedDisplayTime.
+		*/
 		
+		t_atom a[4];
+
+		if (ts.StatusFlags & (ovrStatus_OrientationTracked | ovrStatus_PositionTracked))
+		{
+			ovrPoseStatef posestate = ts.HeadPose;
+			ovrPosef pose = posestate.ThePose;
+			ovrVector3f p = pose.Position;
+			ovrQuatf q = pose.Orientation;
+			//
+			atom_setfloat(a + 0, p.x);
+			atom_setfloat(a + 1, p.y);
+			atom_setfloat(a + 2, p.z);
+			outlet_anything(outlet_msg, gensym("position"), 3, a);
+		}
+
+		/*
+		// Render Scene to Eye Buffers
+		// TODO: calculate these values and pass them to Jitter to capture the scenes:
+		//The code then computes view and projection matrices and sets viewport scene rendering for each eye. In this example, view calculation combines the original pose (originPos and originRot values) with the new pose computed based on the tracking state and stored in the layer. There original values can be modified by input to move the player within the 3D world.
+		for (int eye = 0; eye < 2; eye++) {
+			// Get view and projection matrices for the Rift camera
+			Vector3f pos = originPos + originRot.Transform(layer.RenderPose[eye].Position);
+			Matrix4f rot = originRot * Matrix4f(layer.RenderPose[eye].Orientation);
+
+			Vector3f finalUp      = rot.Transform(Vector3f(0, 1, 0));
+			Vector3f finalForward = rot.Transform(Vector3f(0, 0, -1));
+			Matrix4f view         = Matrix4f::LookAtRH(pos, pos + finalForward, finalUp);
+        
+			Matrix4f proj = ovrMatrix4f_Projection(layer.Fov[eye], 0.2f, 1000.0f, ovrProjection_RightHanded);
+			// Render the scene for this eye.
+			DIRECTX.SetViewport(layer.Viewport[eye]);
+			roomScene.Render(proj * view, 1, 1, 1, 1, true);
+		}
+	*/
+	}
+
+	void jit_gl_texture(t_symbol * s) {
+
+		void * texob = jit_object_findregistered(s);
+		if (!texob) return;	// no texture to copy from.
+
+		long glid = jit_attr_getlong(texob, ps_glid);
+
+		post("got texture %s id %ld\n", s->s_name, glid);
+
+		// Increment to use next texture, just before writing
+		pTextureSet->CurrentIndex = (pTextureSet->CurrentIndex + 1) % pTextureSet->TextureCount;
+		// TODO? Clear and set up render-target.    
+
+		// TODO: copy our texture input to this layer
+
+		// Submit frame with one layer we have.
+		// ovr_SubmitFrame returns once frame present is queued up and the next texture slot in the ovrSwapTextureSet is available for the next frame. 
+		ovrLayerHeader* layers = &layer.Header;
+		ovrResult       result = ovr_SubmitFrame(session, frameIndex, nullptr, &layers, 1);
+		if (result == ovrError_DisplayLost) {
+			/*
+			TODO: If you receive ovrError_DisplayLost, the device was removed and the session is invalid. 
+			Release the shared resources (ovr_DestroySwapTextureSet), destroy the session (ovr_Destory),
+			recreate it (ovr_Create), and create new resources (ovr_CreateSwapTextureSetXXX). 
+			The application's existing private graphics resources do not need to be recreated unless 
+			the new ovr_Create call returns a different GraphicsLuid.
+			*/
+			object_error(&ob, "fatal error connection lost.");
+		}
+
+		// TODO: enable this for multi-threaded rendering
+		// frameIndex++;
+	}
+
+	void perf() {
+		// just toggle through the various perf modes
+		// see https://developer.oculus.com/documentation/pcsdk/latest/concepts/dg-hud/
+		perfMode = (perfMode + 1) % ovrPerfHud_Count;
+		ovr_SetInt(session, OVR_PERF_HUD_MODE, perfMode);
 	}
 };
 
-void * example_new(t_symbol *s, long argc, t_atom *argv) {
-	Example *x = NULL;
-	if ((x = (Example *)object_alloc(max_class))) {
+void * oculusrift_new(t_symbol *s, long argc, t_atom *argv) {
+	oculusrift *x = NULL;
+	if ((x = (oculusrift *)object_alloc(max_class))) {
 		
-		x = new (x) Example();
+		x = new (x) oculusrift();
 		
 		// apply attrs:
 		attr_args_process(x, (short)argc, argv);
 		
 		// invoke any initialization after the attrs are set from here:
-		
+
 	}
 	return (x);
 }
 
-void example_free(Example *x) {
-	x->~Example();
+void oculusrift_free(oculusrift *x) {
+	x->~oculusrift();
 }
 
-void example_assist(Example *x, void *b, long m, long a, char *s)
+void oculusrift_assist(oculusrift *x, void *b, long m, long a, char *s)
 {
 	if (m == ASSIST_INLET) { // inlet
 		sprintf(s, "I am inlet %ld", a);
@@ -58,15 +327,96 @@ void example_assist(Example *x, void *b, long m, long a, char *s)
 	}
 }
 
+void oculusrift_info(oculusrift * x) {
+	x->info();
+}
+
+void oculusrift_bang(oculusrift * x) {
+	x->bang();
+}
+
+void oculusrift_perf(oculusrift * x) {
+	x->perf();
+}
+
+void oculusrift_jit_gl_texture(oculusrift * x, t_symbol * s, long argc, t_atom * argv) {
+	if (argc > 0 && atom_gettype(argv) == A_SYM) {
+		x->jit_gl_texture(atom_getsym(argv));
+	}
+}
+
+// Application Loop:
+//  - Call ovr_GetPredictedDisplayTime() to get the current frame timing information.
+//  - Call ovr_GetTrackingState() and ovr_CalcEyePoses() to obtain the predicted
+//    rendering pose for each eye based on timing.
+//  - Increment ovrTextureSet::CurrentIndex for each layer you will be rendering to 
+//    in the next step.
+//  - Render the scene content into ovrTextureSet::CurrentIndex for each eye and layer
+//    you plan to update this frame. 
+//  - Call ovr_SubmitFrame() to render the distorted layers to the back buffer
+//    and present them on the HMD. If ovr_SubmitFrame returns ovrSuccess_NotVisible,
+//    there is no need to render the scene for the next loop iteration. Instead,
+//    just call ovr_SubmitFrame again until it returns ovrSuccess. ovrTextureSet::CurrentIndex 
+//    for each layer should refer to the texure you want to display.
+//
+
+void oculusrift_quit() {
+	ovr_Shutdown();
+}
+
+void oculusrift_log(int level, const char* message) {
+	post("oculus log %d %s", level, message);
+}
+
 void ext_main(void *r)
 {
 	t_class *c;
-	
-	c = class_new("example", (method)example_new, (method)example_free, (long)sizeof(Example),
+	ovrResult result;
+
+	ps_glid = gensym("glid");
+
+	result = ovr_Initialize(NULL);
+	if (OVR_FAILURE(result)) {
+		error( "LibOVR: failed to initialize library");
+		switch (result) {
+			case ovrError_Initialize: object_error(NULL, "Generic initialization error."); break;
+			case ovrError_LibLoad: object_error(NULL, "Couldn't load LibOVRRT."); break;
+			case ovrError_LibVersion: object_error(NULL, "LibOVRRT version incompatibility."); break;
+			case ovrError_ServiceConnection: object_error(NULL, "Couldn't connect to the OVR Service."); break;
+			case ovrError_ServiceVersion: object_error(NULL, "OVR Service version incompatibility."); break;
+			case ovrError_IncompatibleOS: object_error(NULL, "The operating system version is incompatible."); break;
+			case ovrError_DisplayInit: object_error(NULL, "Unable to initialize the HMD display."); break;
+			case ovrError_ServerStart:  object_error(NULL, "Unable to start the server. Is it already running?"); break;
+			case ovrError_Reinitialization: object_error(NULL, "Attempted to re-initialize with a different version."); break;
+			default: object_error(NULL, "unknown initialization error."); break;
+		}
+		return;
+
+		/*
+		// was crashy:
+		ovrErrorInfo errInfo;
+		ovr_GetLastErrorInfo(&errInfo);
+		object_error(NULL, errInfo.ErrorString);
+
+		}
+		
+		//
+		*/
+	}
+	quittask_install((method)oculusrift_quit, NULL);
+
+	c = class_new("oculusrift", (method)oculusrift_new, (method)oculusrift_free, (long)sizeof(oculusrift),
 				  0L /* leave NULL!! */, A_GIMME, 0);
 	
 	/* you CAN'T call this from the patcher */
-	class_addmethod(c, (method)example_assist,			"assist",		A_CANT, 0);
+	class_addmethod(c, (method)oculusrift_assist,			"assist",		A_CANT, 0);
+
+
+	class_addmethod(c, (method)oculusrift_jit_gl_texture, "jit_gl_texture", A_GIMME, 0);
+
+	class_addmethod(c, (method)oculusrift_info, "info", 0);
+	class_addmethod(c, (method)oculusrift_bang, "bang", 0);
+	class_addmethod(c, (method)oculusrift_perf, "perf", 0);
 	
 	class_register(CLASS_BOX, c);
 	max_class = c;
