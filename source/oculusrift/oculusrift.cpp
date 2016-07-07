b9132b4656c84dc97cdce30c5d3de1b2021695e4
Graham Wakefield <grrrwaaa@gmail.com>
2016-07-07 13:29:02 +0900

    oculusrift: @tracking_level attribute to use floor or eye level refernece

--- source/oculusrift/oculusrift.cpp
+++ source/oculusrift/oculusrift.cpp
@@ -203,11 +203,11 @@ public:
 			// FloorLevel will give tracking poses where the floor height is 0
 			// Tracking system origin reported at floor height.
 			// Prefer using this origin when your application requires the physical floor height to match the virtual floor height, such as standing experiences. When used, all poses in ovrTrackingState are reported as an offset transform from the profile calibrated floor pose. Calling ovr_RecenterTrackingOrigin will recenter the X & Z axes as well as yaw, but the Y-axis (i.e. height) will continue to be reported using the floor height as the origin for all poses.
-			ovr_SetTrackingOriginType(session, ovrTrackingOrigin_FloorLevel);
+			ovr_SetTrackingOriginType(session, ovrTrackingOrigin_FloorLevel);			
 			break;
 		default:
 			// Tracking system origin reported at eye (HMD) height.
-			// Prefer using this origin when your application requires matching user's current physical head pose to a virtual head pose without any regards to a the height of the floor. Cockpit-based, or 3rd-person experiences are ideal candidates. When used, all poses in ovrTrackingState are reported as an offset transform from the profile calibrated or recentered HMD pose. It is recommended that apps using this origin type call ovr_RecenterTrackingOrigin prior to starting the VR experience, but notify the user before doing so to make sure the user is in a comfortable pose, facing a comfortable direction.
+			// Prefer using this origin when your application requires matching user's current physical head pose to a virtual head pose without any regards to a the height of the floor. Cockpit-based, or 3rd-person experiences are ideal candidates. When used, all poses in ovrTrackingState are reported as an offset transform from the profile calibrated or recentered HMD pose. 
 			ovr_SetTrackingOriginType(session, ovrTrackingOrigin_EyeLevel);
 		};
 
