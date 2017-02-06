84a676ede5134d9eedc3fa53f0dadf4fb740193d
Graham Wakefield <grrrwaaa@gmail.com>
2017-02-06 16:33:48 -0500

    windows compile

--- source/compile/compile.cpp
+++ source/compile/compile.cpp
@@ -20,7 +20,9 @@ public:
 	Compile() {
 		outlet_result = outlet_new(&ob, 0);
 		
-		code_string = string_new("extern \"C\" int test(int i) { return -i; }");
+		code_string = string_new("#include <stddef.h> \n object_post(0, __VA_ARGS__) \n extern \"C\" int test(int i) { return -i; }");
+
+
 	}
 	
 	~Compile() {
@@ -33,24 +35,39 @@ public:
 	void test(t_atom_long i) {
 		
 		clang = (t_object *)object_new(CLASS_NOBOX, gensym("clang"), gensym("ctest"));
+
+
 		if (clang != NULL) {
-			object_method(clang, gensym("addsymbol"), gensym("post"), &post);
+			// set C or C++
+			object_attr_setlong(clang, gensym("cpp"), 1);
+
+			// add include paths:
+			//		object_method(clang, gensym("include"), gensym("path/to/include"));
+			//		object_method(clang, gensym("system_include"), gensym("path/to/include")); // for <includes>
 			
+
+			// define macros:
 #ifdef WIN_VERSION
 			object_method(clang, gensym("define"), gensym("WIN_VERSION"));
 #endif
 #ifdef MAC_VERSION
 			object_method(clang, gensym("define"), gensym("MAC_VERSION"));
 #endif
-			
-			object_attr_setlong(clang, gensym("cpp"), 1);
+
+			// compile options:
 			object_attr_setlong(clang, gensym("vectorize"), 1);
 			object_attr_setlong(clang, gensym("fastmath"), 1);
+
+			// push specific symbols:
+			object_method(clang, gensym("addsymbol"), gensym("object_post"), &object_post);
 			
 			t_atom rv, av;
 			atom_setobj(&av, code_string);
 			object_method_typed(clang, gensym("compile"), 1, &av, &rv);
 			int err = atom_getlong(&rv);
+
+			// or, read bitcode:
+			// object_method_sym(clang, gensym("readbitcode"), gensym("path to bitcode"));
 			
 			if (err == 0) {
 				
@@ -60,7 +77,16 @@ public:
 				// but, must keep it around for as long as any code generated is potentially in use
 				// only when all functions become unreachable is it safe to object_release(clang)
 				object_method(clang, gensym("jit"));
+
+				// posts to Max console a list of the functions in the module
+				//object_method(clang, gensym("listfunctions"));
+
+				// post IR header
+				//object_method(clang, gensym("dump"));
 				
+				// write bitcode:
+				//object_method(clang, gensym("writebitcode"), gensym("path to bitcode"));
+
 				// Get a function pointer:
 				t_atom fun_atom;
 				object_method_sym(clang, gensym("getfunction"), gensym("test"), &fun_atom);
@@ -72,6 +98,16 @@ public:
 				} else {
 					object_error(&ob, "couldn't get function");
 				}
+
+				// there is also a "getglobal" that works in the same way as getfunction
+
+				//t_atom ret_atom;
+				//object_method_sym(clang, gensym("getdatalayout"), gensym("test"), &ret_atom);
+				//post("data layout %s", atom_getsym(&ret_atom)->s_name);
+				//object_method_sym(clang, gensym("gettargettriple"), gensym("test"), &ret_atom);
+				//post("target triple %s", atom_getsym(&ret_atom)->s_name);
+				//object_method_sym(clang, gensym("getmoduleid"), gensym("test"), &ret_atom);
+				//post("module id %s", atom_getsym(&ret_atom)->s_name);
 				
 			} else {
 				object_release(clang);
